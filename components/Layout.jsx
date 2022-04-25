b748e1844f4394094b98b4ae211921269fedea03
Topmewan <anon8733@gmail.com>
2022-04-25 19:08:58 +0500

    active links nextjs

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -3,7 +3,7 @@ import Navbar from "./Navbar";
 
 const Layout = ({children}) => {
   return (
-    <div>
+    <div className='layout'>
       <Navbar/>
       {children}
     </div>
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,11 +1,42 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Link from "next/link";
+import {useRouter} from "next/router";
+import styles from '../styles/Navbar.module.css';
 
 const Navbar = () => {
+  const router = useRouter();
+  const [active, setActive] = useState(false);
+
+  const activeLink = (r) => {
+    if (r === router.pathname) {
+      return styles.active
+    }
+    return ''
+  }
   return (
-    <div>
-      Navbardfsfs
+    <div className={styles.wrapper}>
+      <div className={styles.logo}>
+        <Link href='/'>Home</Link>
+      </div>
+
+      <ul className={styles.buttons}>
+        <li>
+          <Link href='/cart'>
+            <a className={`${activeLink('/cart')}`}>Cart</a>
+          </Link>
+        </li>
+        <li>
+          <Link href='/signin'>
+            <a className={`${activeLink('/signin')}`}>
+              Sign In
+            </a>
+          </Link>
+        </li>
+      </ul>
+
     </div>
-  );
+  )
+
 };
 
 export default Navbar;
